afbeb79798c28a4d9d8e540aa5941d8f73cc026c
daniilgorr <daniilgorr@outlook.com>
2023-10-10 07:33:57 +0800

    Добавьте файлы проекта.

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+
+using namespace std;
+float num1{}, num2{};
+
+
+void numberdata()
+{
+    cout << "Введите числа: " << endl;
+    cin >> num1 >> num2;
+}
+
+void symbolsdata()
+{
+    char action;
+    cout << "Выберите действие, которое необходимо выполнить: +, -, *, /" << endl;
+    cin >> action;
+
+    switch (action)
+    {
+    case '+':
+        cout << "result = " << num1 + num2 << endl;
+        break;
+    case '-':
+        cout << "result = " << num1 - num2 << endl;
+        break;
+    case '*':
+        cout << "result = " << num1 * num2 << endl;
+        break;
+    case '/':
+        cout << "result = " << num1 / num2 << endl;
+        break;
+    default:
+        cout << "Invalid choice" << endl;
+        symbolsdata();
+        break;
+    }
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    numberdata();
+    symbolsdata();
+
+    cout << "\t" << "\t" << "\t" << "\t" << "\t" << "\t" << " КАЛЬКУЛЯТОР" << "\t" << endl;
+
+    return 0;
+}
+
+
+
+
+
